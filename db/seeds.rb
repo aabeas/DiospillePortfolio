8cde70c2aa424a2a7937165bd2fb924af04b1f55
aabeas <aaron.beasley@gmail.com>
2018-01-05 19:39:20 -0600

    Implemented initial design for portfolio index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ puts "5 skills created"
     subtitle: "Ruby on Rails",
     body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://via.placeholder.com/600x400",
-    thumb_image: "http://via.placeholder.com/350x200"
+    thumb_image: "http://via.placeholder.com/356x280"
   )
 end
 
@@ -42,7 +42,7 @@ puts "8 portfolio items created"
     subtitle: "Angular",
     body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://via.placeholder.com/600x400",
-    thumb_image: "http://via.placeholder.com/350x200"
+    thumb_image: "http://via.placeholder.com/356x280"
   )
 end
 
