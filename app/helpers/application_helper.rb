c300c5ab9b831a5229b11f20f96a628866aa3108
aabeas <aaron.beasley@gmail.com>
2018-01-16 02:03:29 -0600

    Implemented styles and link for tech news page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,6 +35,10 @@ module ApplicationHelper
         title: 'Contact'
       },
       {
+        url: tech_news_path,
+        title: 'News'
+      },
+      {
         url: blogs_path,
         title: 'Blog'
       },
