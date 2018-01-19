caa2d9430d05041cefd27cf01b524f7ceca72551
aabeas <aaron.beasley@gmail.com>
2018-01-18 19:07:58 -0600

    Created a comment broadcast, configured the websocket route and updated the comment model with a callback

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -7,7 +7,7 @@ module SocialTool
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
 
-    client.search("#esri & #bigdata", result_type: 'recent').take(6).collect do |tweet|
+    client.search("#automl OR #cloud OR #tech", result_type: 'mixed').take(6).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
