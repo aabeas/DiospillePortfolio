fb918d0ee45feb5c9934ba72bda8680449d12b9a
aabeas <aaron.beasley@gmail.com>
2018-01-24 23:59:56 -0600

    Implemented fixes for portfolio image items and source helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contect me', contact_path} if you would like to collaborate."
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path} if you would like to collaborate."
       content_tag(:div, greeting.html_safe, class: styles)
     end
   end
